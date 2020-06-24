c938f4a718f5439feb290a2d09729603032617bd
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-06-24 20:06:33 +0200

    Javascript Added

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    mybutton = document.getElementById("myBtn");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+}    
+
+function scrollToTop(scrollDuration) {
+    var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval);
+    },15);
+}
